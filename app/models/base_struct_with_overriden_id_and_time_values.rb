3223e53e9df649eb77192c99462fbe9a56206090
Arindam Chakraborty <arismart99@gmail.com>
2021-09-06 13:05:34 +0530

    comments

--- app/models/base_struct_with_overriden_id_and_time_values.rb
+++ app/models/base_struct_with_overriden_id_and_time_values.rb
@@ -1,3 +1,9 @@
+# This is a struct factory, which will create a new struct with the required attributes.
+#
+# @params <attrs> - Array of attributes for the model
+# @returns
+#   Returns a struct with the provided attributes as its members.
+#   The returned struct becomes the value of the caller
 BaseStructWithOverridenIdAndTimeValues = -> (*attrs) do
   Struct.new(*attrs) do
     def id
